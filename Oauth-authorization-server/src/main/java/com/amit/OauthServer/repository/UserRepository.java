ae96f3db7ae309eb8ea0e3a8b99099bedb95b975
Amit Bhardwaj <amitbhk2002@gmail.com>
2025-10-08 22:35:32 +0530

    initialize the Oauth authorization server for the client

---
+++ Oauth-authorization-server/src/main/java/com/amit/OauthServer/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.amit.OauthServer.repository;
+
+import com.amit.OauthServer.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    public User findByEmail(String email);
+}
---
+++ Oauth-authorization-server/src/test/java/com/amit/OauthServer/OauthAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amit.OauthServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
